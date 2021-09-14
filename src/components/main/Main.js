9f3588a26f64d28eb1c79c267c2c9bb1ae7a5bb8
nrbns357 <gusgh357@gmail.com>
2021-09-14 20:57:20 +0900

    upDate

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,35 +7,70 @@ import React from '../../asset/img/react.svg';
 import Php from "../../asset/img/php.svg";
 import Mysql from "../../asset/img/mysql.svg";
 
+
 const Main = () => {
     return (
-        <>
+        <div>
         <header className="header">
-            <nav className="navbar">
-                <a href="#mainImg">Home</a>
-                <a href="#AboutMe">AboutMe</a>
-            </nav>
+
+
+                <ul className="header-ul">
+
+                    <li>
+
+                        <a href="#mainImg">Home</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="#AboutMe">AboutMe</a>
+
+                    </li>
+
+                    <li>
+                        <a href="#Skills">Skills</a>
+                    </li>
+
+                </ul>
+
+
         </header>
 
         <main>
+
             <div id="mainImg" className="mainImg">
+
                 <span>Geum Hyunho</span>
+
                 <span>PORTFOLIO<br/>VOL.1</span>
+
                 <span>Front</span><br/>
+
                 <span>Developer</span>
+
             </div>
                 
             <div id="AboutMe" className="AboutMe">
+
                     <div className="Square">
+
                         <span>About Me</span>
+
                         <span>2005.03.04</span>    
 
-                        <img src={Person}/>
+                        <img alt="it's me" src={Person}/>
+
                     </div>
+
                 <div className="AboutMeContent">
+
                     <pre>이름: 금현호</pre>
+
                     <pre>나이: 17세</pre>
+
                 </div>
+
             </div>
 
             <div id="Skills" className="Skills">
@@ -57,7 +92,7 @@ const Main = () => {
             </div>
            
         </main>
-        </>
+        </div>
     );
 };
 
