810b4b9a139722d36463a1eb1a5af0128c829ec0
nrbns357 <gusgh357@gmail.com>
2021-09-15 18:16:31 +0900

    upDate

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,16 +2,16 @@ import "./Footer.css";
 import Insta from "../../asset/img/instagram.svg";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="footerDiv">
-                    <a href="https://www.instagram.com/hyeonho.gold/">
-                        <img src={Insta}/> 
-                    </a>
-                <span>eamil : ghh357@naver.com</span>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="footerDiv">
+        <a href="https://www.instagram.com/hyeonho.gold/">
+          <img src={Insta} />
+        </a>
+        <span>eamil : ghh357@naver.com</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,111 +1,93 @@
-import './Main.css';
+import "./Main.css";
 import Person from "../../asset/img/v.jpg";
 import Html from "../../asset/img/html.svg";
 import Css from "../../asset/img/css.svg";
 import Js from "../../asset/img/javascript.svg";
-import React from '../../asset/img/react.svg';
+import React from "../../asset/img/react.svg";
 import Php from "../../asset/img/php.svg";
 import Mysql from "../../asset/img/mysql.svg";
 
 
 const Main = () => {
-    return (
-        <div>
-        <header className="header">
+  return (
+    <div>
+      <header className="header">
+        <ul className="header-ul">
+          <li>
+            <a href="#mainImg">Home</a>
+          </li>
 
-                <ul className="header-ul">
+          <li>
+            <a href="#AboutMe">AboutMe</a>
+          </li>
 
-                    <li>
+          <li>
+            <a href="#Skills">Skills</a>
+          </li>
 
-                        <a href="#mainImg">Home</a>
+          <li>
+            <a href="#projectBox">Project</a>
+          </li>
+        </ul>
+      </header>
 
-                    </li>
+      <main>
+        <div id="mainImg" className="mainImg">
+          <span>Geum Hyunho</span>
 
-                    <li>
+          <span>
+            PORTFOLIO
+            <br />
+            VOL.1
+          </span>
 
-                        <a href="#AboutMe">AboutMe</a>
+          <span>Front</span>
+          <br />
 
-                    </li>
-
-                    <li>
-                        <a href="#Skills">Skills</a>
-                    </li>
-
-                    <li>
-                        <a href="#projectBox">Project</a>
-                    </li>
-                </ul>
-
-
-        </header>
-
-        <main>
-
-            <div id="mainImg" className="mainImg">
-
-                <span>Geum Hyunho</span>
-
-                <span>PORTFOLIO<br/>VOL.1</span>
-
-                <span>Front</span><br/>
-
-                <span>Developer</span>
-
-            </div>
-                
-            <div id="AboutMe" className="AboutMe">
-
-                    <div className="Square">
-
-                        <span>About Me</span>
-
-                        <span>2005.03.04</span>    
-
-                        <img alt="it's me" src={Person}/>
-
-                    </div>
-
-                <div className="AboutMeContent">
-
-                    <pre>이름: 금현호</pre>
-
-                    <pre>나이: 17세</pre>
-
-                </div>
-
-            </div>
-
-            <div id="Skills" className="Skills">
-                    <span>I can do</span>
-                <div className="skillbox">
-                        <img alt="htmlimg" src={Html}/>
-
-                        <img alt="htmlimg" src={Css}/>
-
-                        <img alt="htmlimg" src={Js}/>
-
-                        <img alt="htmlimg" src={React}/>
-
-                        <img alt="htmlimg" src={Php}/>
-
-                        <img alt="htmlimg" src={Mysql}/>
-                </div>
-
-            </div>
-
-            <div id="projectBox" className="projectBox">
-                <div className="nrbns-server">
-
-                </div>
-
-                <div className="nomad-javascript">
-
-                </div>
-            </div>
-           
-        </main>
+          <span>Developer</span>
         </div>
-    );
+
+        <div id="AboutMe" className="AboutMe">
+          <div className="Square">
+            <span>About Me</span>
+
+            <span>2005.03.04</span>
+
+            <img alt="it's me" src={Person} />
+          </div>
+
+          <div className="AboutMeContent">
+            <pre>이름: 금현호</pre>
+
+            <pre>나이: 17세</pre>
+          </div>
+        </div>
+
+        <div id="Skills" className="Skills">
+          <span>I can do</span>
+          <div className="skillbox">
+            <img alt="htmlimg" src={Html} />
+
+            <img alt="htmlimg" src={Css} />
+
+            <img alt="htmlimg" src={Js} />
+
+            <img alt="htmlimg" src={React} />
+
+            <img alt="htmlimg" src={Php} />
+
+            <img alt="htmlimg" src={Mysql} />
+          </div>
+        </div>
+
+        <div id="projectBox" className="projectBox">
+          <div className="nrbns-server"></div>
+
+          <div className="nomad-javascript"></div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Main;
